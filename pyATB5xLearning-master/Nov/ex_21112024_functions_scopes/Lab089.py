ac18e9f647ce517ddb24623613f40fb45eff3883
SuprajaQA <psupraja11ss@gmail.com>
2024-11-26 12:41:47 +0530

    test

---
+++ pyATB5xLearning-master/Nov/ex_21112024_functions_scopes/Lab089.py
@@ -0,0 +1,9 @@
+#n = int(input("enter number \n"))
+n=5
+for i in range(n):
+#    for j in range(i,n):
+#        print("*",end=" ")# decreasing
+    for j in range(i+1):
+        print("*",end=" ")#increasing
+    print()
+
